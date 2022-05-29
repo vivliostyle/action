ad0c7488b5da33a3d1076b62014d57da70460b97
spring-raining <harusamex.com@gmail.com>
2022-05-30 04:00:27 +0900

    feat: Output persistence

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import * as artifact from '@actions/artifact';
 import * as core from '@actions/core';
 import { exec } from '@actions/exec';
 
 async function run() {
   const input = core.getInput('input');
   const version = core.getInput('vivliostyle-version');
+  const artifactName = core.getInput('artifact-name');
 
   await exec('npx', [
     '@vivliostyle/cli',
@@ -15,6 +17,14 @@ async function run() {
     '--image',
     `ghcr.io/vivliostyle/cli:${version}`,
   ]);
+
+  const artifactClient = artifact.create();
+  const uploadResult = await artifactClient.uploadArtifact(
+    artifactName,
+    ['.'], // TODO: Selective file upload
+    '.'
+  );
+  core.setOutput('upload', uploadResult);
 }
 
 run();
